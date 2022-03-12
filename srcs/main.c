43d15d4ef7817c5a39c4c07694e2381a9c4204d8
Deloise Tentacool <dtentaco@student.21-school.ru>
2022-03-12 13:56:10 +0300

    Init new branch dtentaco-parser / 1

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dtentaco <dtentaco@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dtentaco <dtentaco@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 21:03:39 by dtentaco          #+#    #+#             */
-/*   Updated: 2022/03/11 23:16:34 by dtentaco         ###   ########.fr       */
+/*   Updated: 2022/03/12 13:54:43 by dtentaco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main(int argc, char **argv)
 {
 
-	t_minilibx	mlx;
+	// t_minilibx	mlx;
 
 	(void)argc;
 	(void)argv;
